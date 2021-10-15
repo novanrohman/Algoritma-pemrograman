ce80084a6ca1d68c23627b1a6f02d387125d4705
novanrohman <novanrohman@gmail.com>
2021-10-15 20:49:09 +0700

    update

---
+++ praktikum3/coba.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class coba{
+    public static void main (String[] args){
+        Scanner name = new Scanner(System.in);
+        System.out.println("Masukkan Nama: ");
+
+        String name
+        System.out.println("Hallo" + name + "!");
+    }
+}
