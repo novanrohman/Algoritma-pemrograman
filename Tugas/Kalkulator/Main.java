4b27b694818b5ac47269b79aa1da914f77a15d2a
novanrohman <novanrohman@gmail.com>
2021-11-24 18:50:26 +0700

    update

---
+++ Tugas/Kalkulator/Main.java
@@ -0,0 +1,95 @@
+//Ngobar kalkulator
+import java.lang.Math;
+import java.util.Scanner;
+import java.util.*;
+
+public class Main{
+  public static void main (String[] args){
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Program Kalkulator Sederhana");
+    System.out.println();
+    System.out.println("Pilih");
+    System.out.println("1. Penjumlahan 2. Pengurangan");
+    System.out.println("3. Perkalian  4. Pembagian");
+    System.out.println("5. Sisa bagi");    
+    int choice = input.nextInt();
+
+    switch (choice){
+      case 1:
+      penjumlahan();
+      case 2:
+      pengurangan();
+      case 3:
+      perkalian();
+      case 4:
+      pembagian();
+      case 5:
+      sisa_bagi();
+    }
+  }
+
+    static void penjumlahan(){
+      Scanner math1 = new Scanner(System.in);
+      double a, b, c;
+
+      System.out.println("Masukkan Angka pertama: ");
+      a = math1.nextInt();
+      System.out.println("Masukkan Angka kedua: ");
+      b = math1.nextInt();
+      c = a+b;
+      System.out.println("Hasil =" + c);
+      return;
+    }
+
+    static void pengurangan(){
+      Scanner math1 = new Scanner(System.in);
+      double a, b, c;
+      System.out.println("Masukkan Angka Pertama: ");
+      a = math1.nextInt();
+      System.out.println("Masukkan Angka Kedua: ");
+      b = math1.nextInt();
+      c = a-b;
+      System.out.println("Hasil : " + c);
+      return;
+    }
+
+    static void perkalian(){
+      Scanner math1 = new Scanner(System.in);
+      double a, b, c;
+
+      System.out.println("Masukkan Angka Pertama: ");
+      a = math1.nextInt();
+      System.out.println("Masukkan Angka Kedua: ");
+      b = math1.nextInt();
+      c = a*b;
+      System.out.println("Hasil : " + c);
+      return;
+    }
+
+    static void pembagian(){
+      Scanner math1 = new Scanner(System.in);
+      double a, b, c;
+      
+      System.out.println("Masukkan Angka Pertama: ");
+      a = math1.nextInt();
+      System.out.println("Masukkan Angka Kedua");
+      b = math1.nextInt();
+      c = a / b;
+      System.out.println("Hasil : " + c);
+      return;
+    }
+
+    static void sisa_bagi(){
+      Scanner math1 = new Scanner(System.in);
+      double a, b, c;
+
+      System.out.println("Masukkan Angka Pertama");
+      a = math1.nextInt();
+      System.out.println("Masukkan Angka Kedua");
+      b = math1.nextInt();
+      c = a % b;
+      System.out.println("Hasil : " + c);
+      return;
+    }
+}
