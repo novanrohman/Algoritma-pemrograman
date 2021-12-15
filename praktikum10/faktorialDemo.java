f102b218797720eb8083c8a58c0688583d4d5327
novanrohman <novanrohman@gmail.com>
2021-12-15 09:42:19 +0700

    update

---
+++ praktikum10/faktorialDemo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class faktorialDemo{
+    public static int faktorial(int x){
+        if (x == 0 || x == 1){
+            return 1;
+        } else {
+            return x * faktorial(x-1);
+        }
+    }
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("N = ");
+        int n = input.nextInt();
+        System.out.println("Hasil = " + faktorial(n));
+    }
+}
